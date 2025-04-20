5c8e6a78870b8b188ef52b05ba2f356ebdc833ac
Vision-Tey <nimusiimavision@gmail.com>
2025-04-20 12:23:04 +0300

    Resolve env error

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,6 @@ COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 WORKDIR /var/www/html
 
-COPY .env .env
-
 COPY composer.json composer.lock ./
 RUN composer install
 
--- src/Config/Config.php
+++ src/Config/Config.php
@@ -8,24 +8,18 @@ final class Config
 {
     public static function getConfig(): array
     {
-        // Load environment variables from .env file
-        $dotenv = Dotenv::createImmutable(__DIR__.'/../../');
-        $dotenv->load();
-
-        // Fetch environment variables with provided defaults
-        $host = getenv('MYSQL_HOST') ?: 's9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        $dbname = getenv('MYSQL_DATABASE') ?: 'a5qbyz129p2cinto';
-        $user = getenv('MYSQL_USER') ?: 'hvwpm5dlrcqr34bb';
-        $password = getenv('MYSQL_PASSWORD') ?: 'iplss20sb1qikjfn';
-        $port = getenv('PORT') ?: '3306';
+        if (!isset($_ENV['MYSQL_HOST'])) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+            $dotenv->load();
+        }
 
         return [
             'db' => [
-                'host' => $host,
-                'dbname' => $dbname,
-                'user' => $user,
-                'password' => $password,
-                'port' => $port
+                'host' => $_ENV['MYSQL_HOST'],
+                'dbname' => $_ENV['MYSQL_DATABASE'],
+                'user' => $_ENV['MYSQL_USER'],
+                'password' => $_ENV['MYSQL_PASSWORD'],
+                'port' => $_ENV['PORT']
             ]
         ];
     }
